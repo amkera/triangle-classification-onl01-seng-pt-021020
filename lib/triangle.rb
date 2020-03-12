2b50da0a4ad1452fc904978e094788897ae4fbbc
Amber Kerawala <amber_kerawala@berkeley.edu>
2020-03-12 23:38:47 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -17,7 +17,7 @@ class Triangle
         :equilateral 
       elsif (@side1 == @side2) || (@side2 == @side3) || (@side1 == @side3)
         :isosceles
-      elsif (@side1 != @side2)
+      elsif (@side1 != @side2) &&
   
   class TriangleError < StandardError
     # triangle error code
