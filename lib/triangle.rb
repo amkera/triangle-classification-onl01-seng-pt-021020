6bdf594367dde059a8a44c5bda918a4346ff549f
Amber Kerawala <amber_kerawala@berkeley.edu>
2020-03-12 23:26:24 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,14 +1,9 @@
 class Triangle
   attr_accessor :side1, :side2, :side3
   
-  
   def initialize(all_sides)
     all_sides.each {|key, value} self.send(( "#{key}="), value)}
   end 
-  
-  
-  class TriangleError < Standard Error 
-  end
   
   
 end
