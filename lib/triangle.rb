a2e9d68e10df3120f8c40d0dc588a50fa52ff811
Amber Kerawala <amber_kerawala@berkeley.edu>
2020-03-12 23:38:39 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -10,7 +10,14 @@ class Triangle
   def kind(all_sides)
     if (@side1 <= 0) || (@side2 <= 0) || (@side3<= 0)
       raise TriangleError
-      
+    elsif (@side1 + @side2 <= @side3) || (@side1 + @side3 <= @side2) || (@side2 + @side3 <= @side1)
+      raise TriangleError
+    else 
+      if (@side1 == @side2 == @side3)
+        :equilateral 
+      elsif (@side1 == @side2) || (@side2 == @side3) || (@side1 == @side3)
+        :isosceles
+      elsif (@side1 != @side2)
   
   class TriangleError < StandardError
     # triangle error code
