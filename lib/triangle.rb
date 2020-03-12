bae94856e287bc427fcc847b65e7f4bea73d7a65
Amber Kerawala <amber_kerawala@berkeley.edu>
2020-03-12 23:30:12 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -8,8 +8,8 @@ class Triangle
   def kind(all_sides)
     if (:side1 + :side2) < :side3 || (:side1 + :side3) < :side3 || (:side2 + :side3) < :side1
       begin
-       raise TriangleError
-    rescue 
+        raise TriangleError
+      rescue 
   
   
   class TriangleError < StandardError
