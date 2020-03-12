ab1860ebaeffcb3ab1907dc49da4825ba1751ef3
Amber Kerawala <amber_kerawala@berkeley.edu>
2020-03-12 23:40:37 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -13,7 +13,7 @@ class Triangle
     elsif (@side1 + @side2 <= @side3) || (@side1 + @side3 <= @side2) || (@side2 + @side3 <= @side1)
       raise TriangleError
     else 
-      if (@side1 == @side2 == @side3)
+      if (@side1 == @side2) && (@side2 == @side3)
         :equilateral 
       elsif (@side1 == @side2) || (@side2 == @side3) || (@side1 == @side3)
         :isosceles
