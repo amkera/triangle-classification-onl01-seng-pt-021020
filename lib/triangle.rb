e737ca927720c1373de8bb6679211bc28714d0e3
Amber Kerawala <amber_kerawala@berkeley.edu>
2020-03-12 23:25:37 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,9 +1,9 @@
 class Triangle
   attr_accessor :side1, :side2, :side3
   
-  all_sides = 
   
-  def kind(side1:, side2:, side3)
+  def kind(all_sides)
+    all_sides.each {|key, value} 
   end 
   
   class TriangleError < Standard Error 
